4049284a5841f02e525092e3e77e5ed7cb668ca8
aokealy <aokealy@gmail.com>
2023-07-11 13:00:36 +0000

    create a http response to return core and about page to test urls and views work

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def core(request):
+    return HttpResponse('core page')
+
+def about(request):
+    return HttpResponse('about page')    
