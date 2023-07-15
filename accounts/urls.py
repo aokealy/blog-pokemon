5d181db8848efce0f7ec43d52efd6564a5e527d4
aokealy <aokealy@gmail.com>
2023-07-15 11:08:43 +0000

    create a url for login and create template login and style it using bootstrap and fontawesome

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .import views
+from django.contrib.auth import views as auth_view
 
 urlpatterns = [
      path('register/', views.register, name='register'),
+     path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 ]
