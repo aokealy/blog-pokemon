7e75c21bd6823a6310417980f523aa4fd6410d71
aokealy <aokealy@gmail.com>
2023-07-15 10:12:22 +0000

    create a class called registerfrom and replace usercreationform and remove the help text that is default with django and add email field when registering

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterForm
 
 # Create your views here.
 def register(request):
      if request.method == 'POST':
-          form = UserCreationForm(request.POST)
+          form = RegisterForm(request.POST)
           if form.is_valid():
             form.save()
             return redirect('core')
      else:
-          form = UserCreationForm()  
+          form = RegisterForm()  
              
      context = {
         'form': form,
