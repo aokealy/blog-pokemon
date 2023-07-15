4c9dbee24904b8ec4410d72040721317e9cee62e
aokealy <aokealy@gmail.com>
2023-07-15 17:55:50 +0000

    create model for account and add it to admin page

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.core.validators import FileExtensionValidator
+
 
 # Create your models here.
+class AccountModel(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='account', validators=[FileExtensionValidator(['jpg', 'png'])])
+
+    def __str__(self):
+        return self.user.username
