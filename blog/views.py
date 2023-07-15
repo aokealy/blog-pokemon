d63c92510226a4f40a0881bde2ffc6f2340b8c37
aokealy <aokealy@gmail.com>
2023-07-15 09:19:50 +0000

    create a post form to create blog on core page

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from .models import BlogPostModel
+from .forms import BlogPostModelForm
 
 # Create your views here.
 
 def core(request):
     blogpost = BlogPostModel.objects.all()
-    return render(request, 'blog/core.html', {'blogpost':blogpost})
+    form = BlogPostModelForm()    
+    context = {
+        'blogpost': blogpost,
+        'form': form
+    }
+    return render(request, 'blog/core.html', context)
 
 
