13476ec7d29a0d0b2b3182d6ec24ca28d990db85
aokealy <aokealy@gmail.com>
2023-07-15 10:04:24 +0000

    use post method for register form and redirect to core page when register is complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(request):
-     return render(request, 'accounts/register.html')
+     if request.method == 'POST':
+          form = UserCreationForm(request.POST)
+          if form.is_valid():
+            form.save()
+            return redirect('core')
+     else:
+          form = UserCreationForm()  
+             
+     context = {
+        'form': form,
+    }
+     return render(request, 'accounts/register.html', context)
