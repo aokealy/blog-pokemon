b2c418b8f64d698ff7d04569c563e34d1ccef288
aokealy <aokealy@gmail.com>
2023-07-15 11:21:44 +0000

    create a logout template and link url and add django tags for links on navbar to redirect to pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from django.contrib.auth import views as auth_view
 urlpatterns = [
      path('register/', views.register, name='register'),
      path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+     path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
 ]
